e4b78ef3b38cee836ecce7c09e0267bcefbab7c4
TheMachinist <themachinist@wraithworks.xyz>
2023-05-20 19:52:14 -0400

    Added base ERC20 token for use in staking

---
+++ contracts/mocks/ERC20Mock.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "../tokens/ERC20/BaseERC20.sol";
+
+contract ERC20Mock is BaseERC20 {
+    constructor() BaseERC20("Example", "EXAMPLE") {}
+}
